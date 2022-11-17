31890e98015dffe720301b2a779956bd25f0b66c
Trevor <trevorgrilley@yahoo.com>
2022-11-16 21:35:33 -0500

    fix packages

--- src/app.js
+++ src/app.js
@@ -1,32 +1,53 @@
-import React from 'react';
-import Navbar from './components/Nav';
-import About from './components/About';
-import Portfolio from './components/Work';
-import Contact from './components/Contact';
-import Hero from './components/Hero';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes
-} from "react-router-dom";
+import React, { useState } from 'react';
+import Header from './components/Header'
+import Nav from './components/Nav';
+import ContactForm from './components/Contact';
+import Page from './components/Page';
 import Footer from './components/Footer';
 
-function App() {
 
+function App() {
+  const [contactSelected, setContactSelected] = useState(false);
+  const [categories] = useState([
+    {
+      name: "about me"
+    },
+    {
+      name: "portfolio",
+      
+    },
+    {
+      name: "contact"
+    },
+    {
+      name: "resume",
+      description: "my resume"
+    },
+  ]);
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Hero />
-        <Routes>
-          <Route path="/" element={<About />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/Contact" element={<Contact />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <div>
+      <Header>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
+      </Header>
+      <main>
+        {!contactSelected ? (
+          <>
+            <Page currentCategory={currentCategory}></Page>
+          </>
+          ) : (
+            <ContactForm></ContactForm>
+          )
+        }
+      </main>
+      <Footer></Footer>
+    </div>
   );
 }
 
---
+++ src/componets/contact/index.js
@@ -0,0 +1,2 @@
+import React, { useState } from "react";
+import { validateEmail } from '../../utils/helpers';
